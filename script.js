659a9f0150349048e947b8b4481f0f55a8b1a8ed
Alvaro Guerrero <alvarofutbolguerrero@gmail.com>
2026-02-10 15:11:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 const noBtn = document.getElementById('noBtn');
 const yesBtn = document.getElementById('yesBtn');
 
-// Hacer que el botón "No" huya
+// Botón "No" huye
 noBtn.addEventListener('mouseover', () => {
-    noBtn.style.position = 'fixed';
     const x = Math.random() * (window.innerWidth - noBtn.offsetWidth);
     const y = Math.random() * (window.innerHeight - noBtn.offsetHeight);
     noBtn.style.left = `${x}px`;
     noBtn.style.top = `${y}px`;
 });
 
-// Transición de pantallas al pulsar "Sí"
-document.getElementById('yesBtn').addEventListener('click', () => {
+// Pantallas
+yesBtn.addEventListener('click', () => {
     document.getElementById('question-container').classList.add('hidden');
     document.getElementById('loading-screen').classList.remove('hidden');
 
